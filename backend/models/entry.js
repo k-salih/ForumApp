90aacd3c147ff9bd4f94cb3c6e6d97f0f596fafe
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-01-21 02:39:56 +0300

    Add entry model

---
+++ backend/models/entry.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const entrySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true, minlength: 3 },
+  cretedAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: null },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+})
+
+entrySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Entry = mongoose.model('Entry', entrySchema)
