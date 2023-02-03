4957d8d5d4bffbaae06d04ceeff6325050f22a64
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-02-03 23:40:22 +0300

    Configure error handler middleware to include MongoServerError

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -23,6 +23,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'MongoServerError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
