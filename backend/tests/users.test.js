047402b74b4ff6fca20d338aa9f0d91135496da9
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-02-06 23:10:36 +0300

    Create tests for fail cases

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -37,6 +37,19 @@ describe('When there are some users saved', () => {
 
       expect(resultUser.body).toEqual(userToView)
     })
+
+    test('fails with statuscode 404 if user does not exist', async () => {
+      const validNonexistingId = '63dd1e990793eabf876854fb'
+
+      await api.get(`/api/users/${validNonexistingId}`).expect(404)
+    })
+
+    test('fails with statuscode 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
+      const response = await api.get(`/api/users/${invalidId}`).expect(400)
+
+      expect(response.body.error).toContain('malformatted id')
+    })
   })
 
   describe('user creation', () => {
@@ -61,6 +74,51 @@ describe('When there are some users saved', () => {
       expect(response.body.followers).toEqual([])
       expect(response.body.following).toEqual([])
     })
+
+    test('fails if password is not entered', async () => {
+      const userToCreate = {
+        username: 'testuser3',
+        email: 'testuser3@gmail.com',
+      }
+
+      const response = await api
+        .post('/api/users')
+        .send(userToCreate)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(response.body.error).toContain('Password is required')
+    })
+
+    test('fails if password is less than 6 characters', async () => {
+      const userToCreate = {
+        username: 'testuser3',
+        email: 'testuser3@gmail.com',
+        password: 'test',
+      }
+
+      const response = await api
+        .post('/api/users')
+        .send(userToCreate)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(response.body.error).toContain('Password must be at least 6 characters long')
+    })
+
+    test('fails with status code 400 if username is invalid', async () => {
+      const userToCreate = {
+        username: 'te',
+        email: 'testuser3@gmail.com',
+        password: 'test21',
+      }
+
+      const response = await api
+        .post('/api/users')
+        .send(userToCreate)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+    })
   })
 
   describe('user deletion', () => {
@@ -77,6 +135,13 @@ describe('When there are some users saved', () => {
       const usernames = usersAtEnd.map((u) => u.username)
 
       expect(usernames).not.toContain(userToDelete.username)
+    })
+
+    test('fails with status code 404 if user is not found', async () => {
+      const validNonexistingId = '63dd1e990793eabf876854fb'
+
+      const response = await api.delete(`/api/users/${validNonexistingId}`).expect(404)
+      expect(response.body.error).toContain('User not found')
     })
   })
 
@@ -100,6 +165,38 @@ describe('When there are some users saved', () => {
 
       expect(usernames).toContain(updatedUser.username)
       expect(usernames).not.toContain(userToUpdate.username)
+    })
+
+    test('succeeds when updating password', async () => {
+      const usersAtStart = await helper.usersInDb()
+      const userToUpdate = usersAtStart[0]
+
+      const updatedUser = {
+        ...userToUpdate,
+        password: 'updatedpassword',
+      }
+
+      await api.put(`/api/users/${userToUpdate.id}`).send(updatedUser).expect(200)
+    })
+
+    test('fails with status code 400 if password is less than 6 characters', async () => {
+      const usersAtStart = await helper.usersInDb()
+      const userToUpdate = usersAtStart[0]
+
+      const updatedUser = {
+        ...userToUpdate,
+        password: 'test',
+      }
+
+      const response = await api
+        .put(`/api/users/${userToUpdate.id}`)
+        .send(updatedUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+
+      expect(response.body.error).toContain('Password must be at least 6 characters long')
     })
   })
 })
