70889fa459dcfb30fa75ad0712bf1016cc76506f
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-02-14 00:25:40 +0300

    Create tests for titles

---
+++ backend/tests/titles.test.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose'
+import app from '../app'
+import request from 'supertest'
+
+import Title from '../models/title'
+import User from '../models/user'
+import Entry from '../models/entry'
+
+import helper from './test_helper'
+
+const api = request(app)
+
+describe('when there is initially some titles saved', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    await Entry.deleteMany({})
+    await Title.deleteMany({})
+
+    for (const title of helper.initialTitles) {
+      const titleObject = new Title(title)
+      await titleObject.save()
+    }
+  })
+
+  test('all titles are returned, and in json format', async () => {
+    const response = await api
+      .get('/api/titles')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(helper.initialTitles.length)
+    expect(response.body.map((title) => title.name)).toContain('Ankara')
+    expect(response.body.map((title) => title.name)).toContain('İstanbul')
+  })
+
+  describe('viewing a specific title', () => {
+    test('succeeds with a valid id', async () => {
+      const titlesAtStart = await helper.titlesInDb()
+      const titleToView = titlesAtStart[0]
+      const resultTitle = await api
+        .get(`/api/titles/${titleToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultTitle.body).toEqual(titleToView)
+    })
+
+    test('fails with statuscode 404 if title does not exist', async () => {
+      const validNonexistingId = '63e61af3843af521b04fd708'
+      await api.get(`/api/titles/${validNonexistingId}`).expect(404)
+    })
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
