098f042baffa78fe6c9dcbcbcbc58a26400b0baa
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-03-06 16:09:53 +0300

    Add user component to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import loginService from "./services/login";
 import storageService from "./services/storage";
 import titleService from "./services/titles";
 import entryService from "./services/entries";
+import userService from "./services/users";
 
 import LoginForm from "./components/LoginForm";
 import PopularTitles from "./components/PopularTitles";
 import Title from "./components/Title";
 import Entry from "./components/Entry";
+import User from "./components/User";
 
 function App() {
   const [user, setUser] = useState(null);
   const [titles, setTitles] = useState([]);
   const [entries, setEntries] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const user = storageService.loadUser();
@@ -25,6 +29,7 @@ function App() {
   useEffect(() => {
     titleService.getAll().then((titles) => setTitles(titles));
     entryService.getAll().then((entries) => setEntries(entries));
+    userService.getAll().then((users) => setUsers(users));
   }, []);
 
   const login = async (username, password) => {
@@ -61,6 +66,7 @@ function App() {
         />
         <Route path={`/titles/:name`} element={<Title titles={titles} />} />
         <Route path={"/entries/:id"} element={<Entry entries={entries} />} />
+        <Route path={"/users/:username"} element={<User users={users} />} />
       </Routes>
     </Router>
   );
