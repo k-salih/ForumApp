38d65168c5d839178a7738b2f62dafefbe251adb
Salih Kahyaoglu <salihkahyaoglu@gmail.com>
2023-03-19 23:19:09 +0300

    Correct entryservice by adding authorization

--- frontend/src/services/entries.js
+++ frontend/src/services/entries.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const baseUrl = "http://localhost:3001/api/entries";
+const authorization = `Bearer ${
+  JSON.parse(window.localStorage.getItem("forumappUser")).token
+}`;
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
@@ -13,13 +16,18 @@ const create = async (newObject) => {
 };
 
 const update = async (id, newObject) => {
-  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  const config = {
+    headers: { Authorization: authorization },
+  };
+  const response = await axios.put(`${baseUrl}/${id}`, newObject, config);
   return response.data;
 };
 
 const remove = async (id) => {
-  const response = await axios.delete(`${baseUrl}/${id}`);
-  return response.data;
+  const config = {
+    headers: { Authorization: authorization },
+  };
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
 };
 
 const entryService = { getAll, create, update, remove };
